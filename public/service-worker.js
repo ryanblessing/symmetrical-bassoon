e54ce0912a51e565b7127d07d50f3366b3088e70
Ryan Blessing <ryanblessing20@yahoo.com>
2021-08-19 11:53:20 -0500

    added service worker- and manifest, took from the module work and class work, gotta go back, still not uploading correctly when i go back online

---
+++ public/service-worker.js
@@ -0,0 +1,92 @@
+//Uncomment the lines below to set up the cache files
+
+const CACHE_NAME = 'my-site-cache-v1';
+const DATA_CACHE_NAME = 'data-cache-v1';
+
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/public/css/style.css',
+    '/public/icons/icon-72x72.pgn',
+    '/public/icons/icon-96x96.pgn',
+    '/public/icons/icon-128x128.pgn',
+    '/public/icons/icon-144x144.pgn',
+    '/public/icons/icon-152x152.pgn',
+    '/public/icons/icon-192x192.pgn',
+    '/public/icons/icon-384x384.pgn',
+    '/public/icons/icon-512x512.pgn',
+    '/public/js/idb.js',
+    '/public/js/index.js'
+];
+
+// Install the service worker
+// YOUR CODE HERE
+self.addEventListener('install', function (evt) {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('you files were pre-cached successfully')
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    );
+    self.skipWaiting();
+})
+
+// Activate the service worker and remove old data from the cache
+// YOUR CODE HERE
+self.addEventListener('activate', function (evt) {
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log('removing old cache data', key);
+                        return caches.delete(key);
+                    }
+                })
+            )
+        })
+    )
+    self.ClientRectList.claim();
+})
+
+// Intercept fetch requests
+// YOUR CODE HERE
+self.addEventListener('fetch', function (evt) {
+    if (evt.request.url.includes('/api/')) {
+        evt.respondWith(
+            caches
+            .open(DATA_CACHE_NAME)
+            .then(cache => {
+                return fetch(evt.request)
+                    .then(response => {
+                        // If the response was good, clone it and store it in the cache.
+                        if (response.status === 200) {
+                            cache.put(evt.request.url, response.clone());
+                        }
+
+                        return response;
+                    })
+                    .catch(err => {
+                        // Network request failed, try to get it from the cache.
+                        return cache.match(evt.request);
+                    });
+            })
+            .catch(err => console.log(err))
+        );
+
+        return;
+    }
+    evt.respondWith(
+        fetch(evt.request).catch(function () {
+            return caches.match(evt.request).then(function (response) {
+                if (response) {
+                    return response;
+                } else if (evt.request.headers.get('accept').includes('text/html')) {
+                    // return the cached home page for all requests for html pages
+                    return caches.match('/');
+                }
+            });
+        })
+    );
+})
